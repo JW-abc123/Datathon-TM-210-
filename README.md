2fb71ee3ab12cb7bd90cd07aad6736ee4790fcd0
Kien Khai Duong <jyduongkhai99@gmail.com>
2025-10-09 23:50:04 +0800

    Add case-by-case explanation with SHAP

--- README.md
+++ README.md
@@ -35,3 +35,29 @@ After running the inference, the predictions would be printed to ```predictions.
 - 1 = Suspect
 - 2 = Pathologic
 
+### Case-by-case Explanation
+
+By default, after running the command above, the program will draw a SHAP plot explaining the first sample (first row) in the data.csv file: how each feature contributes to the first sample's predicted class.
+
+The command line can be used to further customize this behaviour:
+
+```bash
+python inference.py data.csv [class_idx=-1] [sample_idx=0]
+```
+where:
+* ```class_idx```: the class for which the SHAP plot will show (```0```, ```1```, or ```2```). When ```class_idx=-1``` (by default), the SHAP plot shows how each feature contributes to the sample's **predicted class**.
+* ```sample_idx```: the index of the sample to draw the SHAP plot for. By default, it is ```0```, representing the first sample in data.csv.
+
+For example,
+```bash
+python inference.py data.csv 2
+```
+
+will draw the SHAP plot for the PATHOLOGIC class in the model's prediction for the first sample, and
+
+
+```bash
+python inference.py data.csv -1 1
+```
+
+will draw the SHAP plot for the predicted class of the second sample.
--- inference.py
+++ inference.py
@@ -1,13 +1,48 @@
 import xgboost as xgb
 import pandas as pd
 import numpy as np
+import shap
 import sys
+import matplotlib.pyplot as plt
+
+classes = ['Normal', 'Suspect', 'Pathological']
 
 def load_model(model_path="xgb_opt.json"):
     """Load a trained XGBoost model from JSON."""
     model = xgb.Booster()
     model.load_model(model_path)
     return model
+
+def explain(model, X, preds, class_idx=-1, sample_idx = 0):
+    explainer = shap.Explainer(model)
+    shap_exp = explainer(X)
+
+    sample = X.iloc[sample_idx]
+    values = shap_exp.values[sample_idx] # shape (features, classes)
+    base_values = shap_exp.base_values[sample_idx] 
+    
+    title = f"SHAP Waterfall Plot for Sample {sample_idx}, Class "
+    if class_idx == -1:
+        class_idx = preds[sample_idx]
+        title = title + classes[class_idx].upper() + " (Predicted)"
+    else:
+        title = title + classes[class_idx].upper()
+
+    # Draw plot for class_idx
+    plt.figure(figsize=(10, 6))
+    shap.plots.waterfall( 
+        shap.Explanation(
+            values=values[:, class_idx], 
+            base_values=base_values[class_idx],
+            data=sample,
+            feature_names=X.columns, 
+        ),
+        max_display=15,
+        show = False
+    )
+
+    plt.title(title)
+    plt.show()
 
 def predict(model, X):
     """Make predictions on a Pandas DataFrame or NumPy array."""
@@ -29,11 +64,19 @@ if __name__ == "__main__":
     # Load your CTG-like data
     data = pd.read_csv(data_path)
 
-    # ⚠️ Make sure to drop label columns if they exist
+    # Make sure to drop label columns if they exist
     X = data.drop(columns=["target"], errors="ignore")
 
     model = load_model(model_path)
-    preds = predict(model, X)
+    preds = predict(model, X).astype(int)
+
+    class_idx = -1
+    sample_idx = 0
+
+    if len(sys.argv) >= 3: class_idx = int(sys.argv[2])  # Optional class index for explanation
+    if len(sys.argv) == 4: sample_idx = int(sys.argv[3])  # Optional sample index for explanation
+    
+    explain(model, X, preds, class_idx, sample_idx)
 
     # Output predictions
     print("Predictions:")
